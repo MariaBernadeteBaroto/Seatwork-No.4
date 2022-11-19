8d9eaa0bcd0fead4b819984c92dfd8a31941b665
MariaBernadete Baroto <bernadetebaroto@gmail.com>
2022-11-19 11:53:06 +0800

    identify only the movements who satisfy condition

--- securitycam.py
+++ securitycam.py
@@ -14,7 +14,13 @@ while cam.isOpened():
     _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
     dilated = cv2.dilate(thresh, None, iterations=3)
     contours, _= cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    cv2.drawContours(frame1, contours, -1, (0, 255, 0), 2)
+    # cv2.drawContours(frame1, contours, -1, (0, 255, 0), 2)
+    # spot only the movements who satisfy condition
+    for c in contours:
+        if cv2.contourArea(c) <5000:
+            continue
+        x, y, w, h = cv2.boundingRect(c)
+        cv2.rectangle(frame1, (x, y), (x+w, y+h), (0, 255, 0), 2)
     if cv2.waitKey(10) == ord('q'):
         break
     cv2.imshow('Computer Cam', frame1)
