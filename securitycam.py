98a1ebb31fe0e9897f3fbfdcd94fc47abe1c6c23
MariaBernadete Baroto <bernadetebaroto@gmail.com>
2022-11-19 11:33:02 +0800

    Successgully initialized the computer camera

--- securitycam.py
+++ securitycam.py
@@ -4,6 +4,14 @@
 import cv2
 
 #Initialize computer camera
+cam = cv2.VideoCapture(0)
+while cam.isOpened():
+    ret, frame1 = cam.read()
+    if cv2.waitKey(10) == ord('q'):
+        break
+    cv2.imshow('Computer Cam', frame1)
+
+
 
 #Identifyiing movements
 #Initializing warning sound
