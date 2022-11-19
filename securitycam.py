795f3eb0e1ca3e6db42845b467ff1ada183c6d0c
MariaBernadete Baroto <bernadetebaroto@gmail.com>
2022-11-19 11:39:30 +0800

    enhanced diff to gray color

--- securitycam.py
+++ securitycam.py
@@ -7,11 +7,12 @@ import cv2
 cam = cv2.VideoCapture(0)
 while cam.isOpened():
     ret, frame1 = cam.read()
-    ret, frame2 = cam.read()     #Identifying movements
-    diff = cv2.absdiff(frame1, frame2)
+    ret, frame2 = cam.read()     
+    diff = cv2.absdiff(frame1, frame2)   #Identifying movements
+    gray = cv2.cvtColor(diff, cv2.COLOR_RGB2GRAY)
     if cv2.waitKey(10) == ord('q'):
         break
-    cv2.imshow('Computer Cam', diff)
+    cv2.imshow('Computer Cam', gray)
 
 
  
