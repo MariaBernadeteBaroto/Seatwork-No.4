1f28140dba204e30888a37924b748ea31339f3ef
MariaBernadete Baroto <bernadetebaroto@gmail.com>
2022-11-19 11:36:59 +0800

    Identifying movements

--- securitycam.py
+++ securitycam.py
@@ -7,11 +7,12 @@ import cv2
 cam = cv2.VideoCapture(0)
 while cam.isOpened():
     ret, frame1 = cam.read()
+    ret, frame2 = cam.read()     #Identifying movements
+    diff = cv2.absdiff(frame1, frame2)
     if cv2.waitKey(10) == ord('q'):
         break
-    cv2.imshow('Computer Cam', frame1)
+    cv2.imshow('Computer Cam', diff)
 
 
-
-#Identifyiing movements
+ 
 #Initializing warning sound
